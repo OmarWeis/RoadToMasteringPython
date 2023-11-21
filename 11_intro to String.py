1cbf9f67d98616ae386fad79c3de81d20144e91b
Omar Weis <132832338+OmarWeis@users.noreply.github.com>
2023-11-21 09:22:54 +0300

    Add files via upload

---
+++ 11_intro to String.py
@@ -0,0 +1,33 @@
+# ---------------11--------------
+myStringOne = 'This is Single Quote'
+myStringTwo = "This is Double Quotes"
+
+print(myStringOne)
+print(myStringTwo)
+
+myStringThree = 'This is Single Quote "Test"'
+myStringFour = "This is Double Quotes 'Test'"
+
+print(myStringThree)
+print(myStringFour)
+
+myStringFive = '''First
+Second 'Test' "Test"
+Third'''
+
+myStringSix = """First
+Second "Test" \\\ 'Test'
+Third"""
+
+print(myStringFive)
+print(myStringSix)
+
+# ---------------------------------
+# Strings Indexing & Slicing
+# [1] All Data in Python is Object
+# [2] Object Contain Elements
+# [3] Every Element Has Its Own Index
+# [4] Python Use Zero Based Indexing ( Index Start From Zero )
+# [5] Use Square Brackets To Access Element
+# [6] Enable Accessing Parts Of Strings, Tuples or Lists
+# ---------------------------------
---
+++ 2_comment in python.py
@@ -0,0 +1,13 @@
+
+# This is Comment
+print("I Love Python")  # This is Inline Comment
+print("Programming")  # I Used This Method Because of Bla Bla Bla
+print("Programming")  # If You Used Test Method Will Through Error
+
+"""
+This is
+Multiple
+Line
+Comments
+
+"""
